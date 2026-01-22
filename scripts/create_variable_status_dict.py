c941228b144c1d4bc6058158ad54df7c39ecf34f
mo-laurenboon <lauren.boon@metoffice.gov.uk>
2026-01-22 15:04:47 +0000

    #34: refactor functions and global constants

--- scripts/create_variable_status_dict.py
+++ scripts/create_variable_status_dict.py
@@ -1,4 +1,4 @@
-# (C) British Crown Copyright 2025, Met Office.
+# (C) British Crown Copyright 2026, Met Office.
 # Please see LICENSE.md for license details.
 """
 This script generates the variable status dictionaries for each model/source.
@@ -6,38 +6,12 @@ This script generates the variable status dictionaries for each model/source.
 from pathlib import Path
 import json
 from difflib import get_close_matches
+import sys
 
+from scripts.common import read_json
 
-def open_json(path: Path) -> dict:
-    """Opens a single JSON file as a dictionary.
-
-    Parameters
-    ----------
-    path: Path
-        The path of the file to be opened.
-
-    Returns
-    -------
-    dict
-        The JSON file content.
-
-    Raises
-    ------
-    FileNotFoundError
-        If the file does not exist at the given path.
-    json.JSONDecodeError
-        If the JSON file structure is invalid.
-    """
-    try:
-        with open(path, "r") as f:
-            file = json.load(f)
-
-    except FileNotFoundError:
-        print(f"File not found: {path}.")
-    except json.JSONDecodeError as err:
-        print(f"Invalid JSON formatting: {err}")
-
-    return file
+REF_INFO_DIR = Path("reference_information")
+MAPPINGS_FILE_LOCATION = REF_INFO_DIR / "mappings.json"
 
 
 def get_all_models(mappings_dict: list[dict]) -> set:
@@ -88,6 +62,44 @@ def get_variable_status(mappings_dict: list[dict], model: str) -> dict:
     return variable_status_dict
 
 
+def get_model_to_override(mappings_dict: list[dict]) -> str:
+    """Returns the model whos variable status dictionary the user wishes to override.
+
+    Parameters
+    ----------
+    mappings_dict: list[dict]
+        The dictionary containing the mapping and model information for all variables.
+
+    Returns
+    -------
+    model: str
+        The model to override.
+    """
+    model = input("Input the model for whos variables you wish to override: ")
+    all_models = get_all_models(mappings_dict)
+    if model not in all_models:
+        print(f"Model not regognised, did you mean {get_close_matches(model, all_models)[0]}?")
+        get_model_to_override(mappings_dict)
+
+    return model
+
+
+def get_overriding_status() -> str:
+    """Returns the status that the variables in the chosen model status dictionary will be overridden with.
+
+    Returns
+    -------
+    status: str
+        The new status of the variables. Can be "approved", "do-not-produce" or "embargoed".
+    """
+    status = input(f"What status would you like to assign all of the variables with? ")
+    if status not in ["approved", "do-not-produce", "embargoed"]:
+        print(f"Status '{status}' not regognised, please choose from 'approved', 'do-not-produce' or 'embargoed'")
+        get_overriding_status()
+
+    return status
+
+
 def save_json(model: str, dictionary: dict) -> None:
     """Saves a single dictionary to JSON format.
 
@@ -98,72 +110,90 @@ def save_json(model: str, dictionary: dict) -> None:
     dictionary: dict
         A dictionary of each variable in a model and its associated status.
     """
-    filename = f"{model}_variable_status.json"
-    outdir = Path("reference_information")
-    outfile_path = outdir / filename
+    outfile_path = REF_INFO_DIR / f"{model}_variable_status.json"
     with open(outfile_path, "w") as fh:
         json.dump(dictionary, fh, indent=4)
 
 
-def _process_override(mappings_dict):
-    model = input("Input the model for whos variables status you wish to override: ")
-    all_models = get_all_models(mappings_dict)
-    if model not in all_models:
-        print(f"Model not regognised, did you mean {get_close_matches(model, all_models)[0]}?")
-        _process_override(mappings_dict)
-    status = input("What status would you like to assign all variables for this model? ")
-    if status not in ["approved", "do-not-produce", "embargoed"]:
-        print(f"Status not regognised, please choose from 'approved', 'do-not-produce' or 'embargoed'")
-        _process_override(mappings_dict)
+def get_repeat_override_request(mappings_dict: list[dict]) -> None:
+    """Determines if the user would like to override another model variable status dictionary file.
 
-    confirmation = input(
-        f"You have chosen to overide all variables in {model} with the status {status}, proceed? [Y/N] "
-    )
-    if confirmation not in ["Y", "N"]:
-        print("Input not recognised")
-        _process_override(mappings_dict)
-    if confirmation == "Y":
-        dictionary = open_json(Path(f"reference_information/{model}_variable_status.json"))
-        for variable, value in dictionary.items():
-            dictionary[variable] = status
-        save_json(model, dictionary)
-        repeat = input("Do you wish to override another model? ")
-        if repeat not in ["Y", "y", "N", "n"]:
-            print("Input not recognised, exiting script, your previous changes will not be affected...")
-            pass
-        if repeat in ["Y", "y"]:
-            _optional_override(mappings_dict)
-        else:
-            pass
-    elif confirmation == "N":
-        _optional_override(mappings_dict)
+    Parameters
+    ----------
+    mappings_dict: list[dict]
+        The dictionary containing the mapping and model information for all variables.
+    """
+    repeat = input("Do you wish to override another model? ")
+    if repeat not in ["Y", "y", "N", "n"]:
+        print("Input not recognised, exiting script, your previous changes will not be affected...")
+        sys.exit()
+    elif repeat in ["Y", "y"]:
+        process_override(mappings_dict)
+    else:
+        sys.exit()
 
 
-def _optional_override(mappings_dict):
+def request_override(mappings_dict: list[dict]) -> None:
+    """Determines if the user would like to override model variable status dictionary file.
+
+    Parameters
+    ----------
+    mappings_dict: list[dict]
+        The dictionary containing the mapping and model information for all variables.
+    """
     override_request = input(
         "Is a status override required? Note this will affect every variable within the model [Y/N] "
     )
     if override_request not in ["Y", "N"]:
         print("Input not recognised")
-        _optional_override(mappings_dict)
+        request_override(mappings_dict)
     if override_request == "Y":
-        _process_override(mappings_dict)
+        process_override(mappings_dict)
     elif override_request == "N":
-        pass
+        sys.exit()
+
+
+def process_override(mappings_dict) -> None:
+    """Processes the override if requested. The requested override information is printed back to the user for
+    confirmation and, upon approval, the requested file is adjusted as requested. This override affects every variables
+    within the chosen model variable status file. The option is given to the user to override any number of the model
+    files one at a time.
+
+    Parameters
+    ----------
+    mappings_dict: list[dict]
+        The dictionary containing the mapping and model information for all variables.
+    """
+    model = get_model_to_override(mappings_dict)
+    status = get_overriding_status()
+
+    confirmation = input(f"Overide all variables in {model} as {status}, proceed? [Y/N] ")
+    if confirmation not in ["Y", "y", "N", "n"]:
+        print("Input not recognised")
+        process_override(mappings_dict)
+
+    if confirmation == "Y":
+        dictionary = read_json(REF_INFO_DIR / f"{model}_variable_status.json")
+        for variable, value in dictionary.items():
+            dictionary[variable] = status
+        save_json(model, dictionary)
+        get_repeat_override_request(mappings_dict)
+    elif confirmation == "N":
+        request_override(mappings_dict)
 
 
 def generate_variable_status_dictionaries() -> None:
     """The main function responsible for generating the variable status dictionary for each model found in the
     mappings.json.
     """
-    mappings_dict = open_json(Path("reference_information/mappings.json"))
+    mappings_dict = read_json(MAPPINGS_FILE_LOCATION)
     all_models = get_all_models(mappings_dict)
     for model in all_models:
         variable_status_dict = get_variable_status(mappings_dict, model)
         save_json(model, variable_status_dict)
         print(f"Processing variables for {model}..... Done")
 
-    _optional_override(mappings_dict)
+    request_override(mappings_dict)
 
 
 if __name__ == "__main__":
