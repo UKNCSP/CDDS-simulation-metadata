11b6916192954f70ea967d07953903298b1d28c2
mo-laurenboon <lauren.boon@metoffice.gov.uk>
2026-01-23 10:42:19 +0000

    #38: fix ruff errors

--- tests/test_scripts/test_generate_metadata_tables.py
+++ tests/test_scripts/test_generate_metadata_tables.py
@@ -1,7 +1,7 @@
 import unittest
 from pathlib import Path
 
-from scripts.generate_metadata_tables import build_table, generate_html
+from scripts.generate_metadata_tables import build_table
 
 
 class TestBuildTable(unittest.TestCase):
@@ -11,19 +11,11 @@ class TestBuildTable(unittest.TestCase):
                        'Variant Label', 'Start Date', 'End Date'],
                        ['u-dv623', 'UKCM2-LL', 'crum', 'CMIP', 'MOHC', '1pctCO2', 'r2i1p1f1', '1850-01-01T00:00:00Z',
                         '2000-01-01T00:00:00Z', 'u-dv623']]
-        expected = ('<table border=1, id="table_id", class="display">\n  <thead>\n   <tr bgcolor="#E0EEFF">\n     <th>'
-                    'Model Workflow ID</th>\n     <th>Model ID</th>\n     <th>Mass Data Class</th>\n     <th>MIP</th>\n'
-                    '     <th>Institution ID</th>\n     <th>Experiment ID</th>\n     <th>Variant Label</th>\n     <th>'
-                    'Start Date</th>\n     <th>End Date</th>\n   </tr>\n                          <tr class="filters">'
-                    '\n     <th></th>\n     <th></th>\n     <th></th>\n     <th></th>\n     <th></th>\n     <th></th>'
-                    '\n     <th></th>\n     <th></th>\n     <th></th>\n   </tr>\n   </thead>\n  <tr bgcolor="#FFFFFF">'
-                    '\n     <td><a href="https://github.com/UKNCSP/CDDS-simulation-metadata/tree/main/workflow_metadata'
-                    '/u-dv623.cfg">u-dv623</a></td>\n     <td>UKCM2-LL</td>\n     <td>crum</td>\n     <td>CMIP</td>\n  '
-                    '   <td>MOHC</td>\n     <td>1pctCO2</td>\n     <td>r2i1p1f1</td>\n     <td>1850-01-01T00:00:00Z'
-                    '</td>\n     <td>2000-01-01T00:00:00Z</td>\n  </tr>\n</table>\n')
+        with open(Path("tests/test_scripts/data/table.html"), "r") as fh:
+            expected = fh.read()
 
         table = build_table(table_data)
-        self.assertEqual(table, expected, "The html format of the table was not built as expected")
+        self.assertEqual(table, expected, "The html format of the table was not built as expected.")
 
 
 if __name__ == "__main__":
