dcb510862d88f00fb088104fd9d83bd19a7bc219
Lauren Boon <lauren.boon@metoffice.gov.uk>
2026-02-03 11:44:58 +0000

    Fix module not found error

--- scripts/generate_variable_lists.py
+++ scripts/generate_variable_lists.py
@@ -19,8 +19,8 @@ import os
 from itertools import chain
 from pathlib import Path
 
-from scripts.common import read_json
-from scripts.constants import REF_INFO_DIR, MAPPINGS_FILE_LOCATION, KNOWN_ISSUES_DICT_FILE_LOCATION, DR_FILE_LOCATION
+from common import read_json
+from constants import REF_INFO_DIR, MAPPINGS_FILE_LOCATION, KNOWN_ISSUES_DICT_FILE_LOCATION, DR_FILE_LOCATION
 
 
 def set_arg_parser() -> argparse.Namespace:
