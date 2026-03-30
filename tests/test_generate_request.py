ae62f587d85548a84eefde87944b2360d5d2987c
mo-laurenboon <lauren.boon@metoffice.gov.uk>
2026-03-30 10:51:05 +0000

    #72: update tests

--- tests/test_generate_request.py
+++ tests/test_generate_request.py
@@ -4,9 +4,7 @@
 import unittest
 import configparser
 
-from scripts.generate_request import (identify_mip_convert_plugin, update_template_with_metadata,
-                                      update_template_with_data, update_template_with_misc, update_template_with_common,
-                                      update_template_with_conversion, validate_request)
+from scripts.generate_request import identify_mip_convert_plugin, update_request, validate_request
 
 
 class TestIdentifyPlugin(unittest.TestCase):
@@ -55,10 +53,10 @@ class TestIdentifyPlugin(unittest.TestCase):
         self.assertRaises(RuntimeError, identify_mip_convert_plugin, config["metadata"])
 
 
-class TestUpdateTemplateWithMetadata(unittest.TestCase):
+class TestUpdateRequest(unittest.TestCase):
 
-    def setUp(self):
-        self.request = {
+    def test_update_request(self):
+        request = {
             "metadata": {
                 "base_date": "",
                 "branch_method": "",
@@ -78,157 +76,12 @@ class TestUpdateTemplateWithMetadata(unittest.TestCase):
                 "parent_model_id": "",
                 "parent_time_units": "",
                 "parent_variant_label": "",
-            }
-        }
-
-        config = configparser.ConfigParser()
-        config['metadata'] = {
-            "base_date": "1850-01-01T00:00:00Z",
-            "branch_method": "standard",
-            "branch_date_in_child": "1850-01-01T00:00:00Z",
-            "branch_date_in_parent": "1850-01-01T00:00:00Z",
-            "parent_experiment_id": "piControl",
-            "parent_mip": "CMIP",
-            "parent_mip_era": "CMIP7",
-            "parent_model_id": "UKCM2-0-LL",
-            "parent_time_units": "days since 1850-01-01T00:00:00Z",
-            "parent_variant_label": "r1i1p1f1",
-            "calendar": "gregorian",
-            "experiment_id": "historical",
-            "institution_id": "MOHC",
-            "mip": "CMIP",
-            "mip_era": "CMIP7",
-            "variant_label": "r2i1p1f1",
-            "model_id": "UKCM2-0-LL"
-        }
-        self.config = config
-
-        self.expected = {
-            'metadata': {
-                'base_date': '1850-01-01T00:00:00Z',
-                'branch_method': 'standard',
-                'calendar': 'standard',
-                'experiment_id': 'historical',
-                'institution_id': 'MOHC',
-                'license': 'CC-BY-4.0',
-                'mip': 'CMIP',
-                'mip_era': 'CMIP7',
-                'model_id': 'UKCM2-0-LL',
-                'variant_label': 'r2i1p1f1',
-                'branch_date_in_child': '1850-01-01T00:00:00Z',
-                'branch_date_in_parent': '1850-01-01T00:00:00Z',
-                'parent_experiment_id': 'piControl',
-                'parent_mip': 'CMIP',
-                'parent_mip_era': 'CMIP7',
-                'parent_model_id': 'UKCM2-0-LL',
-                'parent_time_units': 'days since 1850-01-01T00:00:00Z',
-                'parent_variant_label': 'r1i1p1f1'
-            }
-        }
-        return self
-
-    def test_update_tempalte_with_metadata_gregorian(self):
-        updated_request = update_template_with_metadata(self.request, self.config["metadata"])
-
-        err_msg = "Failed to correctly populate metadata section of request with a gregorian calendar"
-        self.assertEqual(updated_request, self.expected, err_msg)
-
-    def test_update_template_with_metadata_360day(self):
-        self.config["metadata"]["calendar"] = "360_day"
-        updated_request = update_template_with_metadata(self.request, self.config["metadata"])
-        self.expected["metadata"]["calendar"] = "360_day"
-        err_msg = "Failed to correctly populate metadata section of request with a 360_day calendar"
-        self.assertEqual(updated_request, self.expected, err_msg)
-
-
-class TestUpdateTemplateWithData(unittest.TestCase):
-
-    def setUp(self):
-        self.request = {
-            "data": {
-                "end_date": "",
-                "mass_data_class": "",
-                "mass_ensemble_member": "",
-                "model_workflow_id": "",
-                "output_mass_suffix": "production",
-                "output_mass_root": "moose:/adhoc/projects/cdds",
-                "start_date": "",
-                "streams": "ap4 ap5 ap6 ap7 ap8 ap9 apu apt inm onm ind ond",
-                "variable_list_file": ""
             },
-        }
-
-        config = configparser.ConfigParser()
-        config["metadata"] = {
-            "experiment_id": "historical",
-            "model_id": "UKCM2-0-LL"
-        }
-        config['data'] = {
-            "start_date": "1850-01-01T00:00:00Z",
-            "end_date": "2000-01-01T00:00:00Z",
-            "mass_data_class": "crum",
-            "mass_ensemble_member": "",
-            "model_workflow_id": "u-dv623"
-        }
-        self.config = config
-
-        self.expected = {
-            'data': {
-                'end_date': '2000-01-01T00:00:00Z',
-                'mass_data_class': 'crum',
-                'mass_ensemble_member': '',
-                'model_workflow_id': 'u-dv623',
-                'output_mass_suffix': 'production',
-                'output_mass_root': 'moose:/adhoc/projects/cdds',
-                'start_date': '1850-01-01T00:00:00Z',
-                'streams': 'ap4 ap5 ap6 ap7 ap8 ap9 apu apt inm onm ind ond',
-                'variable_list_file': 'variables/v1.2.2.3/u-dv623_historical_UKCM2-0-LL.txt'
-            }
-        }
-        return self
-
-    def test_update_template_with_data_crum(self):
-        updated_request = update_template_with_data(self.request, self.config["data"], self.config["metadata"])
-        err_msg = "Failed to populate data section of request file with mass data class 'crum'"
-        self.assertEqual(updated_request, self.expected, err_msg)
-
-    def test_update_template_with_data_ens(self):
-        self.config["data"]["mass_data_class"] = "ens"
-        self.config["data"]["mass_ensemble_member"] = "an_id"
-        self.expected["data"]["mass_data_class"] = "ens"
-        self.expected["data"]["mass_ensemble_member"] = "an_id"
-        updated_request = update_template_with_data(self.request, self.config["data"], self.config["metadata"])
-        err_msg = "Failed to populate data section of request file with mass data class 'ens'"
-        self.assertEqual(updated_request, self.expected, err_msg)
-
-
-class TestUpdateTemplateWithMisc(unittest.TestCase):
-
-    def test_update_template_with_misc(self):
-        self.request = {
-            "misc": {
-                "atmos_timestep": ""
-            }
-        }
-        config = configparser.ConfigParser()
-        config["misc"] = {
-            "atmos_timestep": "1800"
-        }
-        self.config = config
-        self.expected = {
-            "misc": {
-                "atmos_timestep": "1800"
-            }
-        }
-        updated_request = update_template_with_misc(self.request, self.config["misc"])
-        err_msg = "Failed to populate misc section of request file"
-        self.assertEqual(updated_request, self.expected, err_msg)
-
-
-class TestUpdateTemplateWithCommon(unittest.TestCase):
-
-    def test_update_template_with_common(self):
-        self.request = {
+            "netcdf_global_attributes": {
+                "data_specs_version": "MIP-DS7.1.0.0",
+                "drs_specs": "MIP-DRS7",
+                "region": "glb"
+            },
             "common": {
                 "external_plugin": "",
                 "external_plugin_location": "",
@@ -245,23 +98,74 @@ class TestUpdateTemplateWithCommon(unittest.TestCase):
                 "standard_names_dir": "$CDDS_ETC/standard_names/",
                 "standard_names_version": "latest",
                 "workflow_basename": ""
+            },
+            "data": {
+                "end_date": "",
+                "mass_data_class": "",
+                "mass_ensemble_member": "",
+                "model_workflow_id": "",
+                "output_mass_suffix": "production",
+                "output_mass_root": "moose:/adhoc/projects/cdds",
+                "start_date": "",
+                "streams": "",
+                "variable_list_file": ""
+            },
+            "misc": {
+                "atmos_timestep": ""
+            },
+            "conversion": {
+                "continue_if_mip_convert_failed": "False",
+                "cylc_args": "-v",
+                "mip_convert_plugin": "",
+                "skip_extract": "False",
+                "skip_extract_validation": "False",
+                "skip_configure": "False",
+                "skip_qc": "False",
+                "skip_archive": "False"
             }
         }
         config = configparser.ConfigParser()
-        config["metadata"] = {
-            "experiment_id": "historical",
-            "model_id": "UKCM2-0-LL",
-            "variant_label": "r2i1p1f1"
+        config.read("tests/test_scripts/data/test_config.cfg")
+        issue_info = {
+            "model_workflow-id": "u-dv623",
+            "streams": "ap4, ap5, ap6",
+            "package_name": "test"
         }
-        self.config = config
-        self.expected = {
+
+        updated_request = update_request(request, config, issue_info)
+        expected = {
+            'metadata': {
+                'base_date': '1850-01-01T00:00:00Z',
+                'branch_method': 'standard',
+                'calendar': 'standard',
+                'experiment_id': '1pctCO2',
+                'institution_id': 'MOHC',
+                'license': 'CC-BY-4.0',
+                'mip': 'CMIP',
+                'mip_era': 'CMIP7',
+                'model_id': 'UKCM2-0-LL',
+                'variant_label': 'r2i1p1f1',
+                'branch_date_in_child': '1850-01-01T00:00:00Z',
+                'branch_date_in_parent': '1850-01-01T00:00:00Z',
+                'parent_experiment_id': 'piControl',
+                'parent_mip': 'CMIP',
+                'parent_mip_era': 'CMIP7',
+                'parent_model_id': 'UKCM2-0-LL',
+                'parent_time_units': 'days since 1850-01-01',
+                'parent_variant_label': 'r1i1p1f1'
+            },
+            'netcdf_global_attributes': {
+                'data_specs_version': 'MIP-DS7.1.0.0',
+                'drs_specs': 'MIP-DRS7',
+                'region': 'glb'
+            },
             'common': {
                 'external_plugin': '',
                 'external_plugin_location': '',
                 'log_level': 'INFO',
                 'mip_table_dir': '$CDDS_ETC/mip_tables/CMIP7/DR-1.2.2.3-v1.0.2',
                 'mode': 'strict',
-                'package': '',
+                'package': 'test',
                 'root_ancil_dir': '$CDDS_ETC/ancil_testing/',
                 'root_hybrid_heights_dir': '$CDDS_ETC/vertical_coordinates/',
                 'root_replacement_coordinates_dir': '$CDDS_ETC/horizontal_coordinates/',
@@ -270,38 +174,35 @@ class TestUpdateTemplateWithCommon(unittest.TestCase):
                 'sites_file': '$CDDS_ETC/cfmip2/cfmip2-sites-orog.txt',
                 'standard_names_dir': '$CDDS_ETC/standard_names/',
                 'standard_names_version': 'latest',
-                'workflow_basename': 'UKCM2-0-LL_historical_r2i1p1f1'
+                'workflow_basename': 'UKCM2-0-LL_1pctCO2_r2i1p1f1'
+            },
+            'data': {
+                'end_date': '2000-01-01T00:00:00Z',
+                'mass_data_class': 'crum',
+                'mass_ensemble_member': '',
+                'model_workflow_id': 'u-dv623',
+                'output_mass_suffix': 'production',
+                'output_mass_root': 'moose:/adhoc/projects/cdds',
+                'start_date': '1850-01-01T00:00:00Z',
+                'streams': 'ap4 ap5 ap6',
+                'variable_list_file': 'variables/v1.2.2.3/u-dv623_1pctCO2_UKCM2-0-LL.txt'
+            },
+            'misc': {
+                'atmos_timestep': '1800'
+            },
+            'conversion': {
+                'continue_if_mip_convert_failed': 'False',
+                'cylc_args': '-v',
+                'mip_convert_plugin': 'UKCM2',
+                'skip_extract': 'False',
+                'skip_extract_validation': 'False',
+                'skip_configure': 'False',
+                'skip_qc': 'False',
+                'skip_archive': 'False'
             }
         }
-
-        updated_request = update_template_with_common(self.request, self.config["metadata"])
-        updated_request["common"]["package"] = ""
-        err_msg = "Failed to populate common section of request file"
-        self.assertEqual(updated_request, self.expected, err_msg)
-
-
-class TestUpdateTemplateWithConversion(unittest.TestCase):
-
-    def test_update_template_with_conversion(self):
-        self.request = {
-            "conversion": {
-                "mip_convert_plugin": ""
-            }
-        }
-        config = configparser.ConfigParser()
-        config["metadata"] = {
-            "model_id": "UKCM2-0-LL"
-        }
-        self.config = config
-        self.expected = {
-             "conversion": {
-                "mip_convert_plugin": "UKCM2"
-            }
-        }
-
-        updated_request = update_template_with_conversion(self.request, self.config["metadata"])
-        err_msg = "Failed to populate conversion section of request file"
-        self.assertEqual(updated_request, self.expected, err_msg)
+        err_msg = f"Failed to update full request file.\nExpected:\n{expected}\nGot:\n{updated_request}"
+        self.assertEqual(updated_request, expected, err_msg)
 
 
 class TestValidateRequest(unittest.TestCase):
