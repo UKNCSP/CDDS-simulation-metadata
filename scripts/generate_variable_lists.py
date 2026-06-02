57dec35b919dc693842170d5e9247596f709db41
mo-laurenboon <lauren.boon@metoffice.gov.uk>
2026-06-02 11:35:59 +0100

    #87: organise vars with unknown status

--- scripts/generate_variable_lists.py
+++ scripts/generate_variable_lists.py
@@ -477,6 +477,8 @@ def sort_key(line: str) -> int:
     """
     if "do-not-produce (not available with this model)" in line:
         return 5
+    elif "unknown (no stream information available)" in line:
+        return 7
     elif "do-not-produce" in line:
         return 6
     elif "known-issue" in line:
