5229677d8542d83c61efadc678f763f042ad486e
mo-laurenboon <lauren.boon@metoffice.gov.uk>
2026-06-02 11:33:03 +0100

    #87: getting mapping file from tagged version

--- scripts/update_mapping_file.py
+++ scripts/update_mapping_file.py
@@ -4,9 +4,10 @@
 import requests
 import json
 
-from scripts.constants import MAPPINGS_FILE_LOCATION
+from scripts.constants import MAPPINGS_FILE_LOCATION, MAPPING_FILE_VERSION
 
-link = "https://raw.githubusercontent.com/UKNCSP/CDDS-CMIP7-mappings/refs/heads/main/data/condensed_mappings.json"
+link = (f"https://raw.githubusercontent.com/UKNCSP/CDDS-CMIP7-mappings/refs/tags/{MAPPING_FILE_VERSION}/data/"
+        "condensed_mappings.json")
 new_mapping_dict = requests.get(link).json()
 
 with open(MAPPINGS_FILE_LOCATION, "w") as f:
